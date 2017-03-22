ccc094e0160f9db85dc9f3d64c15634633194f89
Dipak Thapa <dipakthapaofficial@gmail.com>
2017-03-22 11:22:33 +0545

    initial commit..

---
+++ src/main/java/com/lftechnology/service_commons/App.java
@@ -0,0 +1,13 @@
+package com.lftechnology.service_commons;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
